286a11ff3b1049ec66761f30c626ea8165932aed
Fabrizio Nunnari <fab.nunnari@gmail.com>
2020-09-21 15:46:57 +0200

    Updated model tests.

--- timl/classification/test/test_models.py
+++ timl/classification/test/test_models.py
@@ -7,16 +7,9 @@ from keras.layers import Dense, InputLayer
 
 from ..classifier import Classifier
 
-import pkg_resources
+from timl.common.test.test_augmentation import sample_image
+
 from typing import Tuple
-
-
-@pytest.fixture
-def sample_image() -> Image:
-    image_path = pkg_resources.resource_filename("timl.data", "sample_images/ISIC_0000000.jpeg")
-    img = PIL.Image.open(image_path)
-
-    return img
 
 
 @pytest.fixture(params=[(227, 227), (450, 450)])
@@ -36,35 +29,39 @@ def classifier(request, image_size, n_classes) -> Classifier:
     return out
 
 
-def test_classifier(classifier: Classifier, sample_image, image_size, n_classes):
+def test_classifier(classifier: Classifier, sample_image: Image):
 
     model = classifier.get_model()
     assert model is not None
     assert len(model.layers) > 2
 
     # The input layer must match the image size
+    w, h, depth = classifier.get_input_size()
+    assert depth == 3  # TODO -- This restriction should disappear, one day.
+    assert w == h  # TODO -- This restriction should disappear, one day.
     in_layer: InputLayer = model.layers[0]
     assert type(in_layer) == InputLayer
-    assert in_layer.input_shape == (None, image_size[0], image_size[1], 3)
-    assert in_layer.output_shape == (None, image_size[0], image_size[1], 3)
+    assert in_layer.input_shape == (None, w, h, depth)
+    assert in_layer.output_shape == (None, w, h, depth)
     assert not in_layer.trainable
 
     # The output layer must be a softmax with the correct number of classes.
     out_layer: Dense = model.layers[-1]
+    n_classes = classifier.get_output_size()
     assert type(out_layer) == Dense
     assert out_layer.units == n_classes
+    assert out_layer.units >= 2  # n_classes
     assert out_layer.output_shape == (None, n_classes)
     assert out_layer.trainable
 
-    assert classifier.get_output_size() == n_classes
+    # Perform predictions
+    prediction, confidence = classifier.predict_image(sample_image)
 
-    # load an image an predict
-    img = PIL.Image.open(fp=sample_image)
-    prediction, confidence = classifier.predict_image(img)
-
+    assert len(prediction) == n_classes
     for p in prediction:
         assert 0 <= p <= 1.0
 
+    assert len(confidence) == n_classes
     for c in confidence:
         assert -1 <= c <= 1
 
